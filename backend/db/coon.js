5984ac46d2b555a1d5eaeedf806d28705b50d378
pedrofelipemoreira <pfelope@gmail.com>
2024-05-31 15:51:11 -0300

    definindo a porta

--- backend/db/coon.js
+++ backend/db/coon.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config(); 
 
-const DB_COON = process.env.DB_COON || 3000
+const DB_COON = process.env.DB_COON || "mongodb+srv://Pedro:alyRZaY16v575rDy@cluster0.stnpqrc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 
 async function main(){
